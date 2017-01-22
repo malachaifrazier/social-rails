6141db65e860e6edeb297c5b78c4aaa8fccc2e22
Malachai Frazier <malachaifrazier@gmail.com>
2017-01-22 00:31:17 -0500

    bio removed from FB Oauth

--- config/initializers/omniauth_fb.rb
+++ config/initializers/omniauth_fb.rb
@@ -1,7 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :facebook, Figaro.env.facebook_key, Figaro.env.facebook_secret,
            scope: 'email,public_profile',
-           info_fields: 'name,first_name,last_name,email,about,bio,gender',
+           info_fields: 'name,first_name,last_name,email,about,gender',
            image_size: 'large',
            client_options: {
             site: 'https://graph.facebook.com/v2.8',
